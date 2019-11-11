beb86c29efb65ca408c86507181572873b005fd0
Gram <gram7gram@gmail.com>
2019-11-12 00:09:53 +0200

    #master build

---
+++ owner/build/precache-manifest.5febf85c1e993dd9b2929564081cb6a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89a5f1565d2458936a82d94daf0ade99",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5581f5e988352de77829",
+    "url": "/static/css/2.f53fa76e.chunk.css"
+  },
+  {
+    "revision": "698ec8bd484526f7cb35",
+    "url": "/static/css/main.dd159920.chunk.css"
+  },
+  {
+    "revision": "5581f5e988352de77829",
+    "url": "/static/js/2.cdab101f.chunk.js"
+  },
+  {
+    "revision": "698ec8bd484526f7cb35",
+    "url": "/static/js/main.a055dc00.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "64f0feb7775b00eb45e75ec59c3001e1",
+    "url": "/static/media/avatar-unknown.64f0feb7.png"
+  }
+]);
