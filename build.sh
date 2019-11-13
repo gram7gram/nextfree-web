637347b188b63391686ac527e105a6cbb869a739
Gram <gram7gram@gmail.com>
2019-11-13 22:00:28 +0200

    #master fixed

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-#docker-compose exec owner npm run build:prod
-#test $? -gt 0 && exit 1
-#
-#docker-compose exec customer npm run build:prod
-#test $? -gt 0 && exit 1
-#
-#docker-compose exec staff npm run build:prod
-#test $? -gt 0 && exit 1
+docker-compose exec owner npm run build:prod
+test $? -gt 0 && exit 1
+
+docker-compose exec customer npm run build:prod
+test $? -gt 0 && exit 1
+
+docker-compose exec staff npm run build:prod
+test $? -gt 0 && exit 1
 
 docker-compose exec admin npm run build:prod
 test $? -gt 0 && exit 1
@@ -15,5 +15,5 @@ test $? -gt 0 && exit 1
 git add owner/build customer/build staff/build admin/build
 test $? -gt 0 && exit 1
 
-git commit -m '#master build' --amend
+git commit -m '#master build'
 test $? -gt 0 && exit 1
