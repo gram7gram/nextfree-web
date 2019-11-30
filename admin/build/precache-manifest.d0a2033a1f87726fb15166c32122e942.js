9d0761813616ecb85e915dea8bd88068cef4ca2c
Gram <gram7gram@gmail.com>
2019-11-30 23:31:33 +0200

    #master build

---
+++ admin/build/precache-manifest.d0a2033a1f87726fb15166c32122e942.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92d73445180a289c1e2ba79ba0518d3e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6804ce01c0641f14e707",
+    "url": "/static/css/2.ae19aa34.chunk.css"
+  },
+  {
+    "revision": "aaefa811ed5d1b30f6a4",
+    "url": "/static/css/main.95cd6c9b.chunk.css"
+  },
+  {
+    "revision": "6804ce01c0641f14e707",
+    "url": "/static/js/2.732bcc69.chunk.js"
+  },
+  {
+    "revision": "aaefa811ed5d1b30f6a4",
+    "url": "/static/js/main.14717e10.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "64f0feb7775b00eb45e75ec59c3001e1",
+    "url": "/static/media/avatar-unknown.64f0feb7.png"
+  },
+  {
+    "revision": "b56e3346517fff06aad4e159e377ee63",
+    "url": "/static/media/company-placeholder.b56e3346.png"
+  },
+  {
+    "revision": "de50e2e9ca0fc52b57dbe9600b542e19",
+    "url": "/static/media/loading.de50e2e9.gif"
+  },
+  {
+    "revision": "a308c60e3835825b53111c4c2c705b26",
+    "url": "/static/media/scanner.a308c60e.jpg"
+  },
+  {
+    "revision": "dc4ad0a38787a122d51eac533e9262d1",
+    "url": "/static/media/staff-placeholder.dc4ad0a3.png"
+  },
+  {
+    "revision": "0995afa59d14362fd1960ef31a6343bb",
+    "url": "/static/media/store-placeholder.0995afa5.png"
+  }
+]);
